5d71b472d38d92d4dafbdbb73bb511a4efe5e008
Bjørge Næss <bjoerge@gmail.com>
2015-03-19 15:26:05 +0100

    Fix syntax error

--- test/expectations/3.js
+++ test/expectations/3.js
@@ -32,71 +32,72 @@ export default {
                 innvandrere: {
                   personer: 139,
                   prosent: 39
-              }
-            },
-            "alle": {
-              innvkat5: {
-                alle: {
-                  personer: 1055,
-                  prosent: 61.3
-                },
-                innvandrere: {
-                  personer: 210,
-                  prosent: 53.9
+                }
+              },
+              "alle": {
+                innvkat5: {
+                  alle: {
+                    personer: 1055,
+                    prosent: 61.3
+                  },
+                  innvandrere: {
+                    personer: 210,
+                    prosent: 53.9
+                  }
                 }
               }
             }
           }
         }
       }
-    }
-  },
-  tree: {
-    "2011": {
-      "videregaende_fullfort": {
-        "F1": {
-          "fylke_nr": "1",
-          "innvkat_5": {
-            "alle": {
-              "kjonn": {
-                "0": {
-                  "enhet": {
-                    "personer": "1226",
-                    "prosent": "71,2"
-                  }
-                },
-                "1": {
-                  "enhet": {
-                    "personer": "1055",
-                    "prosent": "61,3"
-                  }
-                },
-                "alle": {
-                  "enhet": {
-                    "personer": "2281",
-                    "prosent": "66,3"
+    },
+    tree: {
+      "2011": {
+        "videregaende_fullfort": {
+          "F1": {
+            "fylke_nr": "1",
+            "innvkat_5": {
+              "alle": {
+                "kjonn": {
+                  "0": {
+                    "enhet": {
+                      "personer": "1226",
+                      "prosent": "71,2"
+                    }
+                  },
+                  "1": {
+                    "enhet": {
+                      "personer": "1055",
+                      "prosent": "61,3"
+                    }
+                  },
+                  "alle": {
+                    "enhet": {
+                      "personer": "2281",
+                      "prosent": "66,3"
+                    }
                   }
                 }
-              }
-            },
-            "innvandrere": {
-              "kjonn": {
-                "0": {
-                  "enhet": {
-                    "personer": "371",
-                    "prosent": "68,3"
-                  }
-                },
-                "1": {
-                  "enhet": {
-                    "personer": "139",
-                    "prosent": "39"
-                  }
-                },
-                "alle": {
-                  "enhet": {
-                    "personer": "210",
-                    "prosent": "53,9"
+              },
+              "innvandrere": {
+                "kjonn": {
+                  "0": {
+                    "enhet": {
+                      "personer": "371",
+                      "prosent": "68,3"
+                    }
+                  },
+                  "1": {
+                    "enhet": {
+                      "personer": "139",
+                      "prosent": "39"
+                    }
+                  },
+                  "alle": {
+                    "enhet": {
+                      "personer": "210",
+                      "prosent": "53,9"
+                    }
                   }
                 }
               }
