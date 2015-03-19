e64d33559c6e9fc6eca880f48e214ade483823ba
Bjørge Næss <bjoerge@gmail.com>
2015-03-19 13:32:53 +0100

    Fix borked syntax in expectation 1

--- test/expectations/1.js
+++ test/expectations/1.js
@@ -1,14 +1,14 @@
 export default {
   query: {
     table: "befolkning_hovedgruppe",
-    region: ["K0102"]
-    time: ["1986"], // "1985", "1989-2015", ["1989","1990","1991"], "1989,1990,1991"
+    region: ["K0102"],
+    time: ["1986"] // "1985", "1989-2015", ["1989","1990","1991"], "1989,1990,1991"
   },
   result: {
     befolkning_hovedgruppe: {
-      time: ['1986'], 
+      time: ['1986'],
       data: {
-        "K0102":
+        "K0102": {
           innvkat5: {
             alle: {
               prosent: [10],
@@ -30,70 +30,68 @@ export default {
               prosent: [13],
               person: [9392]
             }
-          },
+          }
         }
       }
-    },
+    }
   },
   tree: {
     "1986": {
       "befolkning_hovedgruppe": {
-        "kommune": {
-          "102": {
-            "innvkat_5": {
-              "alle": {
-                "enhet": {
-                  "personer": "568"
-                }
-              },
-              "innvandrere": {
-                "enhet": {
-                  "personer": "73"
-                }
-              },
-              "befolkningen_ellers": {
-                "enhet": {
-                  "personer": "5711"
-                }
-              },
-              "norskfodte_m_innvf": {
-                "enhet": {
-                  "personer": "72"
-                }
-              },
-              "bef_u_innv_og_norskf": {
-                "enhet": {
-                  "personer": "9392"
-                }
+        "K0102": {
+          "innvkat_5": {
+            "alle": {
+              "enhet": {
+                "personer": "568"
+              }
+            },
+            "innvandrere": {
+              "enhet": {
+                "personer": "73"
+              }
+            },
+            "befolkningen_ellers": {
+              "enhet": {
+                "personer": "5711"
+              }
+            },
+            "norskfodte_m_innvf": {
+              "enhet": {
+                "personer": "72"
+              }
+            },
+            "bef_u_innv_og_norskf": {
+              "enhet": {
+                "personer": "9392"
               }
             }
-          },
-          "103": {
-            "innvkat_5": {
-              "alle": {
-                "enhet": {
-                  "personer": "94893"
-                }
-              },
-              "innvandrere": {
-                "enhet": {
-                  "personer": "913"
-                }
-              },
-              "befolkningen_ellers": {
-                "enhet": {
-                  "personer": "34562"
-                }
-              },
-              "norskfodte_m_innvf": {
-                "enhet": {
-                  "personer": "342"
-                }
-              },
-              "bef_u_innv_og_norskf": {
-                "enhet": {
-                  "personer": "709382"
-                }
+          }
+        },
+        "K0103": {
+          "innvkat_5": {
+            "alle": {
+              "enhet": {
+                "personer": "94893"
+              }
+            },
+            "innvandrere": {
+              "enhet": {
+                "personer": "913"
+              }
+            },
+            "befolkningen_ellers": {
+              "enhet": {
+                "personer": "34562"
+              }
+            },
+            "norskfodte_m_innvf": {
+              "enhet": {
+                "personer": "342"
+              }
+            },
+            "bef_u_innv_og_norskf": {
+              "enhet": {
+                "personer": "709382"
               }
             }
           }
@@ -101,4 +99,5 @@ export default {
       }
     }
   }
+
 };
