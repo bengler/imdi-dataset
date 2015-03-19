03a2d5b4826474444540a705d7944f1b28b1e836
Even Westvang <even@bengler.no>
2015-03-19 15:26:31 +0100

    Reverse order of test 2

--- test/expectations/3.js
+++ test/expectations/3.js
@@ -2,7 +2,7 @@ export default {
   query: {
     table: "videregaende_fullfort",
     region: ["F1"],
-    dimensions: ["innvkat_5", "kjonn"],
+    dimensions: ["kjonn", "innvkat_5"],
     time: ["2011"]
   },
   result: {
@@ -10,34 +10,37 @@ export default {
       time: ['2011'],
       data: {
         "F1": {
-          innvkat5: {
-            alle: {
-              kjonn: {
-                "0": {
+          kjonn: {
+            "0": {
+              innvkat5: {
+                alle: {
                   personer: 1226,
                   prosent: 71.2
                 },
-                "1": {
-                  personer: 1055,
-                  prosent: 61.3
-                },
-                alle: {
-                  personer: 1055,
-                  prosent: 61.3
+                innvandrere: {
+                  personer: 371,
+                  prosent: 68.3
                 }
               }
             },
-            innvandrere: {
-              kjonn: {
-                "0": {
-                  personer: 371,
-                  prosent: 68.3
+            "1": {
+              innvkat5: {
+                alle: {
+                  personer: 1055,
+                  prosent: 61.3
                 },
-                "1": {
+                innvandrere: {
                   personer: 139,
                   prosent: 39
-                },
+              }
+            },
+            "alle": {
+              innvkat5: {
                 alle: {
+                  personer: 1055,
+                  prosent: 61.3
+                },
+                innvandrere: {
                   personer: 210,
                   prosent: 53.9
                 }
