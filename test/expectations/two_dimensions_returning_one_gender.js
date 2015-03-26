c8d6abd7c42f126a910d9c9e1b24f7addf8c0a08
Even Westvang <even@bengler.no>
2015-03-26 16:34:51 +0100

    Added two tests for returing single dimensions for datasets with multiple

---
+++ test/expectations/two_dimensions_returning_one_gender.js
@@ -0,0 +1,73 @@
+export default {
+  query: {
+    table: "videregaende_fullfort",
+    regions: ["F1"],
+    dimensions: ["kjonn"],
+    time: ["2011"]
+  },
+  result: {
+    table: "videregaende_fullfort",
+    time: ['2011'],
+    data: {
+      "F1": {
+        kjonn: {
+          "0": {
+            "personer": "1226",
+            "prosent": "71,2"
+          },
+          "1": {
+            "personer": "1055",
+            "prosent": "61,3"
+          },
+          "alle": {
+            "personer": "3232",
+            "prosent": "69,3"
+          }
+        }
+      }
+    }
+  },
+  tree: {
+    "2011": {
+      "videregaende_fullfort": {
+        "F1": {
+          "innvkat_5": {
+            "alle": {
+              "kjonn": {
+                "0": {
+                  "personer": "1226",
+                  "prosent": "71,2"
+                },
+                "1": {
+                  "personer": "1055",
+                  "prosent": "61,3"
+                },
+                "alle": {
+                  "personer": "3232",
+                  "prosent": "69,3"
+                }
+              }
+            },
+            "innvandrere": {
+              "kjonn": {
+                "0": {
+                  "personer": "371",
+                  "prosent": "68,3"
+                },
+                "1": {
+                  "personer": "139",
+                  "prosent": "39"
+
+                },
+                "alle": {
+                  "personer": "210",
+                  "prosent": "53,9"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
---
+++ test/expectations/two_dimensions_returning_one_innvkat.js
@@ -0,0 +1,69 @@
+export default {
+  query: {
+    table: "videregaende_fullfort",
+    regions: ["F1"],
+    dimensions: ["innvkat_5"],
+    time: ["2011"]
+  },
+  result: {
+    table: "videregaende_fullfort",
+    time: ['2011'],
+    data: {
+      "F1": {
+        innvkat_5: {
+          alle: {
+            personer: ["3232"],
+            prosent: ["69,3"]
+          },
+          innvandrere: {
+            personer: ["210"],
+            prosent: ["53,9"]
+          }
+        }
+      }
+    }
+  },
+  tree: {
+    "2011": {
+      "videregaende_fullfort": {
+        "F1": {
+          "innvkat_5": {
+            "alle": {
+              "kjonn": {
+                "0": {
+                  "personer": "1226",
+                  "prosent": "71,2"
+                },
+                "1": {
+                  "personer": "1055",
+                  "prosent": "61,3"
+                },
+                "alle": {
+                  "personer": "3232",
+                  "prosent": "69,3"
+                }
+              }
+            },
+            "innvandrere": {
+              "kjonn": {
+                "0": {
+                  "personer": "371",
+                  "prosent": "68,3"
+                },
+                "1": {
+                  "personer": "139",
+                  "prosent": "39"
+
+                },
+                "alle": {
+                  "personer": "210",
+                  "prosent": "53,9"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
