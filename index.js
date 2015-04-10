3db34c888956e014af5fa37401b2cb41ec7eba5f
Bjørge Næss <bjoerge@gmail.com>
2015-04-10 10:22:58 +0200

    Use a top-level data.js instead of require.resolve technique

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 var DB = require("./db");
-module.exports.DATA_FILE = require.resolve("./out/tree.json");
 module.exports.DB = DB;
 module.exports.parseQueryDimension = require("./lib/parseQueryDimension");
